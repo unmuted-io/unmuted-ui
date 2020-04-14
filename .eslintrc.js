a6115ae5b43769760c6a4ac61ca9dc072563a972
kylanhurt <kylan.hurt@gmail.com>
2020-04-14 16:03:38 -0500

    Add React JSX support for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,8 @@ module.exports = {
     },
     "extends": [
         "plugin:@typescript-eslint/recommended",
-        "plugin:@typescript-eslint/recommended-requiring-type-checking"
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+        "tslint-react"
     ],
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
