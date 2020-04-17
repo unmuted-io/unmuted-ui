22831f4660dc5925408194d8864fb0714492fa37
kylanhurt <kylan.hurt@gmail.com>
2020-04-16 22:07:06 -0500

    Include more video info on viewVideo page

--- src/views/viewVideo/viewVideo.tsx
+++ src/views/viewVideo/viewVideo.tsx
@@ -1,13 +1,14 @@
-import React, { Component, Fragment } from "react"
-import { Row, Col, Card, CardHeader, CardBody, Button, FormGroup, Label, Input } from "reactstrap"
-import DropzoneComponent from "react-dropzone-component"
-import InputMask from "react-input-mask"
-import ReactNumeric, { predefinedOptions } from "react-numeric"
-import MainCard from "../../components/mainCard/mainCard"
+import React, { Component, Fragment } from 'react'
+import { Row, Col, Card, CardHeader, CardBody, Button, FormGroup, Label, Input } from 'reactstrap'
+import DropzoneComponent from 'react-dropzone-component'
+import InputMask from 'react-input-mask'
+import ReactNumeric, { predefinedOptions } from 'react-numeric'
+import MainCard from '../../components/mainCard/mainCard'
 import WebSocketChat from '../../components/WebSocketChat/WebSocketChat'
 import { useParams } from 'react-router-dom'
 import EOSIOClient from '../../eosioClient'
 import VideoPlayer from '../../components/videoPlayer/VideoPlayer'
+import { string } from '@amcharts/amcharts4/core'
 
 // const eosClient = new EOSIOClient('haytemrtg4ge') // hardcode app name
 interface ViewVideoComponentProps {
@@ -15,9 +16,12 @@ interface ViewVideoComponentProps {
 }
 
 interface ViewVideoComponentState {
-  title: string,
-  description: string,
+  title: string
+  description: string
   sourceRand: string
+  created_at: string
+  username: string
+  count: number
 }
 
 class ViewVideo extends Component<ViewVideoComponentProps, ViewVideoComponentState> {
@@ -26,7 +30,10 @@ class ViewVideo extends Component<ViewVideoComponentProps, ViewVideoComponentSta
     this.state = {
       title: '',
       description: '',
-      sourceRand: ''
+      sourceRand: '',
+      created_at: '',
+      username: '',
+      count: 0,
     }
   }
 
@@ -37,7 +44,7 @@ class ViewVideo extends Component<ViewVideoComponentProps, ViewVideoComponentSta
     const videoData = await videoResponse.json()
     this.setState({
       ...videoData[0],
-      sourceRand: rand
+      sourceRand: rand,
     })
   }
 
@@ -48,39 +55,53 @@ class ViewVideo extends Component<ViewVideoComponentProps, ViewVideoComponentSta
 
   render() {
     const { REACT_APP_API_BASE_URL } = process.env
-    const { sourceRand, title, description } = this.state
+    const { sourceRand, title, description, created_at, username, count } = this.state
+    if (!sourceRand) return <div />
     const videoPath = `${REACT_APP_API_BASE_URL}/videos/processed/${sourceRand}`
     const videoJsOptions = {
       autoplay: false,
       controls: true,
-      sources: [{
-        src: 'https://coolestguidesontheplanet.com/videodrome/cgp_video/mymoviei.mp4',
-        type: 'video/mp4'
-      }],
+      sources: [
+        {
+          src: 'https://coolestguidesontheplanet.com/videodrome/cgp_video/mymoviei.mp4',
+          type: 'video/mp4',
+        },
+      ],
       fill: true,
-      aspectRatio: '16:9'
+      aspectRatio: '16:9',
     }
-    if (!sourceRand) return (<div />)
+    const date = new Date(created_at)
+    const dtf = new Intl.DateTimeFormat('en', { year: 'numeric', month: 'long', day: '2-digit' })
+    const [{ value: mo },,{ value: da },,{ value: ye }] = dtf.formatToParts(date)
+    const createdAtSyntax = `${mo} ${da}, ${ye}`
+
     return (
-      <Row id='view-video'>
-        <Col sm={12} lg={8} id='video-wrapper'>
+      <Row id="view-video">
+        <Col sm={12} lg={8} id="video-wrapper">
           <Card>
             <CardBody>
-                <VideoPlayer { ...videoJsOptions } sourceRand={sourceRand} />
-                <div id='primary-video-info'>
-                  <p>{description}</p>
-                </div>
-              </CardBody>
-            </Card>
-          </Col>
-          <Col sm={12} lg={4}>
-            <Card>
-              <CardBody style={{ height: 480, display: 'flex', flexDirection: 'row' }}>
-                <WebSocketChat sendSuperChat={this.sendSuperChat} />
-              </CardBody>
-            </Card>
-          </Col>
-        </Row>
+              <VideoPlayer {...videoJsOptions} sourceRand={sourceRand} />
+              <div className="primary-video-info">
+                <h3 className="primary-video-info-title">{title}</h3>
+                <h4 className="primary-video-info-username">{username}</h4>
+                <p className="primary-video-info-description">
+                  <span className="primary-video-info-description-stats">
+                    {count} views | {createdAtSyntax}
+                  </span><br /><br />
+                  <span className="primary-video-info-description-content">{description}</span>
+                </p>
+              </div>
+            </CardBody>
+          </Card>
+        </Col>
+        <Col sm={12} lg={4}>
+          <Card>
+            <CardBody style={{ height: 480, display: 'flex', flexDirection: 'row' }}>
+              <WebSocketChat sendSuperChat={this.sendSuperChat} />
+            </CardBody>
+          </Card>
+        </Col>
+      </Row>
     )
   }
 }
