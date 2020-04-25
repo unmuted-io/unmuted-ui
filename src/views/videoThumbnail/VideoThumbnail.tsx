6d21d875834780d0438720a6d46884e906515a25
kylanhurt <kylan.hurt@gmail.com>
2020-04-25 16:58:14 -0500

    Fix video thumbnail issue

--- src/views/videoThumbnail/VideoThumbnail.tsx
+++ src/views/videoThumbnail/VideoThumbnail.tsx
@@ -7,6 +7,8 @@ import {
 } from '../../utility/utility'
 import PlaceholderImage from "../../assets/images/placeholder.png"
 
+const { REACT_APP_API_BASE_URL } = process.env
+
 export interface VideoThumbnailProps {
   title: string,
   description: string,
@@ -21,10 +23,37 @@ export interface VideoThumbnailProps {
 }
 
 export interface VideoThumbnailState {
-
+  currentThumbnail: string,
+  timer: number
 }
 
 class VideoThumbnail extends React.Component<VideoThumbnailProps, VideoThumbnailState> {
+  hoverInterval: any
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      currentThumbnail: PlaceholderImage,
+      timer: 0
+    }
+    this.hoverInterval
+  }
+  onMouseIn = () => {
+    console.log('mouse in')
+    this.hoverInterval = setInterval(() => {
+      const { timer } = this.state
+      this.setState({
+        timer: timer + 100
+      })
+      console.log('this.state.timer: ', this.state.timer)
+    }, 100)
+  }
+
+  onMouseOut = () => {
+    console.log('mouse out')
+    clearInterval(this.hoverInterval)
+  }
+
   render() {
     const {
       title,
@@ -35,16 +64,21 @@ class VideoThumbnail extends React.Component<VideoThumbnailProps, VideoThumbnail
       count,
       rand
     } = this.props
+    const { currentThumbnail, timer } = this.state
     const shortenedTitle = cutOffText(title, 60)
     const createdAtTime = (new Date(created_at)).getTime()
     const nowTime = (new Date()).getTime()
     const msTimeDifference = nowTime - createdAtTime
     const timeAgo = secondsToHms(msTimeDifference / 1000)
+    const thumbnailTimer = Math.floor(timer / 1000)
+    const thumbnailIterator = (thumbnailTimer % 8) + 1
+    const thumbnailSource = `${REACT_APP_API_BASE_URL}/videos/processed/thumbnails/${source.replace('.mp4', '')}-${thumbnailIterator}.png`
+    console.log('thumbnailSource: ', thumbnailSource)
     return (
       <div className='video-thumbnail'>
         <div className='video-thumbnail-image'>
           <NavLink to={`/videos/${rand}`}>
-            <img className='video-thumbnail-image-content' src={PlaceholderImage} />
+            <img className='video-thumbnail-image-content' src={thumbnailSource} onMouseOver={this.onMouseIn} onMouseOut={this.onMouseOut} />
           </NavLink>
         </div>
         <div className='video-thumbnail-info'>
