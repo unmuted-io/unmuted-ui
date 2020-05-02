1acbef16d5e78f090a3d4af6c84a586955fc590d
kylanhurt <kylan.hurt@gmail.com>
2020-05-02 18:54:49 -0500

    Reformat layout index

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,54 +1,52 @@
-import React, { Component, Suspense } from 'react';
+import React, { Component, Suspense } from 'react'
 import { connect } from 'react-redux'
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom'
 import MainLayout from '../containers/templateConfig/mainLayout'
-import routes from "../extranalRoute";
-import Spinner from '../components/spinner/Spinner';
+import routes from '../extranalRoute'
+import Spinner from '../components/spinner/Spinner'
 import { attemptAutoLoginFromCookies } from '../redux/actions/authActions'
 import { withRouter } from 'react-router-dom'
+import axios from 'axios'
 
 class Layout extends Component {
-    componentWillMount = () => {
-        const { history, attemptAutoLoginFromCookies } = this.props
-        attemptAutoLoginFromCookies(history)
-    }
+  componentWillMount = () => {
+    const { history, attemptAutoLoginFromCookies } = this.props
+    attemptAutoLoginFromCookies(history)
+  }
 
-    render() {
-        const menu = routes.map((route, index) => {
-            return (route.component) ? (
-                <Route
-                    key={index}
-                    path={route.path}
-                    exact={route.exact}
-                    name={route.name}
-                    render={props => (
-                        <route.component {...props} />
-                    )} />
-            ) : (null);
-        });
+  render() {
+    const menu = routes.map((route, index) => {
+      return route.component ? (
+        <Route
+          key={index}
+          path={route.path}
+          exact={route.exact}
+          name={route.name}
+          render={(props) => <route.component {...props} />}
+        />
+      ) : null
+    })
 
-        return (
-            <Suspense fallback={<Spinner />}>
-                <Switch>
-                    {menu}
-                    <Route path="/" component={MainLayout} />
-                </Switch>
-            </Suspense>
-        );
-    }
+    return (
+      <Suspense fallback={<Spinner />}>
+        <Switch>
+          {menu}
+          <Route path="/" component={MainLayout} />
+        </Switch>
+      </Suspense>
+    )
+  }
 }
 
 const mapStateToProps = (state) => {
-    return {
-
-    }
+  return {}
 }
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-        attemptAutoLoginFromCookies: (history) => dispatch(attemptAutoLoginFromCookies(history)),
-        dispatch
-    }
+  return {
+    attemptAutoLoginFromCookies: (history) => dispatch(attemptAutoLoginFromCookies(history)),
+    dispatch,
+  }
 }
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Layout))
