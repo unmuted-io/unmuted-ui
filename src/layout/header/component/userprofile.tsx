edbb50dfda7079c63be818ecc6b80de998a3914e
kylanhurt <kylan.hurt@gmail.com>
2020-05-05 15:08:44 -0500

    Implement small profile thumbnail in dropdown settings menu
    Implement small profile thumbnail in dropdown settings menu

--- src/layout/header/component/userprofile.tsx
+++ src/layout/header/component/userprofile.tsx
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom'
 import avatar from '../../../assets/images/user/avatar-1.jpg'
 import { State, Account } from '../../../types'
 
+const { REACT_APP_API_BASE_URL } = process.env
+
 type UserProfileOwnProps = {
   logout: () => void
 }
@@ -21,16 +23,19 @@ type UserProfileState = {
 class UserProfile extends Component<UserProfileProps, UserProfileState> {
   render() {
     const { logout, account } = this.props
-    const { username } = account
+    const { username, settings } = account
+    const { profileImageUrl = '' } = settings
+    const profileImageSrc = `${REACT_APP_API_BASE_URL}/${profileImageUrl}`
+
     return (
       <Fragment>
         <UncontrolledDropdown className="drp-user">
           <DropdownToggle tag="a" href="#" className="dropdown-toggle">
-            <i className="icon feather icon-settings"></i>
+              <i className="icon feather icon-settings"></i>
           </DropdownToggle>
           <DropdownMenu right className="profile-notification">
             <div className="pro-head">
-              <img src={avatar} className="img-radius" alt="User-Profile" />
+              <img src={profileImageSrc || avatar} className="img-radius dropdown-user-profile-image" alt="User-Profile" />
               <span>{username}</span>
               <Link onClick={logout} to="#" className="dud-logout" title="Logout">
                 <i className="feather icon-log-out" />
