0a621efe14afbdab62947e90d61dcbd15ccf2ce2
kylanhurt <kylan.hurt@gmail.com>
2020-11-23 16:16:59 -0700

    Replace Elite Able logos with Unmuted

--- src/layout/sidebar/navlogo/navLogo.jsx
+++ src/layout/sidebar/navlogo/navLogo.jsx
@@ -1,30 +1,30 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import classnames from 'classnames';
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import classnames from 'classnames'
 import Path from '../../../configure'
+import logo from '../../../assets/images/logo/blacb-bg-logo-no-bg@0.3x.png'
+import unmutedText from '../../../assets/images/logo/white-logo-text@0.3x.png'
 
 class NavLogo extends Component {
   render() {
     return (
       <div className="navbar-brand header-logo">
         <Link to={Path.defaultPath} className="b-brand">
-          <div className="b-bg">
-            E
-          </div>
-          <span className="b-title">
-            Elite Able
-          </span>
+          <img src={logo} style={{ height: 32 }} />
+          <img src={unmutedText} style={{ height: 18, marginLeft: 12 }} />
         </Link>
         <Link
           to="#"
           onClick={() => this.props.collapseMenu()}
-          className={classnames("mobile-menu", {
-            on: this.props.meunCol
+          className={classnames('mobile-menu', {
+            on: this.props.meunCol,
           })}
-        ><span></span></Link>
+        >
+          <span></span>
+        </Link>
       </div>
-    );
+    )
   }
 }
 
-export default NavLogo;
+export default NavLogo
