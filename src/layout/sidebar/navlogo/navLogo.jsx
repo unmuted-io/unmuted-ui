264dc43bfbb950d42c506ba80e631c0cdc0fc908
kylanhurt <kylan.hurt@gmail.com>
2020-11-23 18:27:50 -0700

    Fix header size

--- src/layout/sidebar/navlogo/navLogo.jsx
+++ src/layout/sidebar/navlogo/navLogo.jsx
@@ -5,26 +5,24 @@ import Path from '../../../configure'
 import logo from '../../../assets/images/logo/blacb-bg-logo-no-bg@0.3x.png'
 import unmutedText from '../../../assets/images/logo/white-logo-text@0.3x.png'
 
-class NavLogo extends Component {
-  render() {
-    return (
-      <div className="navbar-brand header-logo">
-        <Link to={Path.defaultPath} className="b-brand">
-          <img src={logo} style={{ height: 32 }} />
-          <img src={unmutedText} style={{ height: 18, marginLeft: 12 }} />
-        </Link>
-        <Link
-          to="#"
-          onClick={() => this.props.collapseMenu()}
-          className={classnames('mobile-menu', {
-            on: this.props.meunCol,
-          })}
-        >
-          <span></span>
-        </Link>
-      </div>
-    )
-  }
+const NavLogo = ({ collapseMenu, meunCol }) => {
+  return (
+    <div className="navbar-brand header-logo">
+      <Link to={Path.defaultPath} className="b-brand">
+        <img className="logo-image" src={logo} />
+        <img className="logo-text" src={unmutedText} />
+      </Link>
+      <Link
+        to="#"
+        onClick={() => collapseMenu()}
+        className={classnames('mobile-menu', {
+          on: meunCol,
+        })}
+      >
+        <span></span>
+      </Link>
+    </div>
+  )
 }
 
 export default NavLogo
