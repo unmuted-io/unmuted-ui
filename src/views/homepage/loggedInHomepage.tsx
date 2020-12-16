1c44bd8cc6fa34d44a391c1a206e36258885feb7
kylanhurt <kylan.hurt@gmail.com>
2020-12-16 06:59:52 -0700

    Add try catch to recommended videos fetch

--- src/views/homepage/loggedInHomepage.tsx
+++ src/views/homepage/loggedInHomepage.tsx
@@ -1,8 +1,5 @@
 import React, { Component } from 'react'
-import {
-  Row,
-  Col
-} from 'reactstrap'
+import { Row, Col } from 'reactstrap'
 import MainCard from '../../components/mainCard/mainCard'
 import { ClipLoader } from 'react-spinners'
 import { connect, Connect } from 'react-redux'
@@ -10,18 +7,18 @@ import VideoThumbnail from '../videoThumbnail/VideoThumbnail'
 import axios from 'axios'
 
 export interface LoggedInHomepageProps {
-  account: any;
+  account: any
 }
 
 export interface LoggedInHomepageState {
-  recommended: any[];
+  recommended: any[]
 }
 
 class LoggedInHomepage extends React.Component<LoggedInHomepageProps, LoggedInHomepageState> {
-  constructor (props: LoggedInHomepageProps) {
+  constructor(props: LoggedInHomepageProps) {
     super(props)
     this.state = {
-      recommended: []
+      recommended: [],
     }
   }
 
@@ -33,23 +30,22 @@ class LoggedInHomepage extends React.Component<LoggedInHomepageProps, LoggedInHo
     const { REACT_APP_API_BASE_URL } = process.env
     const { account } = this.props
     const username = account ? account.username : ''
-    const recommendedVideoResponse = await axios.get(`${REACT_APP_API_BASE_URL}/videos/rec/20/${username}`)
-    this.setState({
-      recommended: recommendedVideoResponse.data.videos
-    })
+    try {
+      const recommendedVideoResponse = await axios.get(`${REACT_APP_API_BASE_URL}/videos/rec/20/${username}`)
+      this.setState({
+        recommended: recommendedVideoResponse.data.videos,
+      })
+    } catch (err) {
+      console.error('fetchRecommendedVideos error')
+    }
   }
 
-  render () {
+  render() {
     const { recommended } = this.state
     return (
-      <MainCard title="Recommended" isOption className='recommended-videos-grid'>
-        {Object.values(recommended).map(video => {
-          return (
-            <VideoThumbnail
-              key={video.rand + Math.floor(Math.random() * 99999)}
-              {...video}
-            />
-          )
+      <MainCard title="Recommended" isOption className="recommended-videos-grid">
+        {Object.values(recommended).map((video) => {
+          return <VideoThumbnail key={video.rand + Math.floor(Math.random() * 99999)} {...video} />
         })}
       </MainCard>
     )
@@ -58,14 +54,12 @@ class LoggedInHomepage extends React.Component<LoggedInHomepageProps, LoggedInHo
 
 const mapStateToProps = (state) => {
   return {
-    account: state.auth.account
+    account: state.auth.account,
   }
 }
 
 const mapDispatchToProps = (dispatch) => {
-  return {
-
-  }
+  return {}
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(LoggedInHomepage)
